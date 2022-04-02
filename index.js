6257083b662c8bb37fe2b8381be1aa7d800a8c4c
Benjamin <shallifi@hotmail.com>
2022-04-01 23:05:26 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    updateArrow();
+
+})
+ /*  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  ) */
+function updateArrow () {
+    document.getElementById("text").innerHTML = "This is really cool!";
+
+}
